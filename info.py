cb5c7a137ff386cbbeb461f29135c6f9e5137d06
owajbhai <owajshaikhrcm@gmail.com>
2025-07-26 21:56:02 +0530

    Update info.py

--- info.py
+++ info.py
@@ -120,7 +120,7 @@ THREE_VERIFY_GAP = int(environ.get('THREE_VERIFY_GAP', "54000"))
 # ============================
 # Channel & Group Links Configuration
 # ============================
-GRP_LNK = environ.get('GRP_LNK', 'https://t.me/MovieSearchAutoGroup') # Group link for the bot
+GRP_LNK = environ.get('GRP_LNK', 'https://t.me/Prime_Movie_YT_Group') # Group link for the bot
 OWNER_LNK = environ.get('OWNER_LNK', 'https://t.me/Prime_Movie_Request_bot') # Owner link for the bot
 UPDATE_CHNL_LNK = environ.get('UPDATE_CHNL_LNK', 'https://t.me/HdHinde2025All') # Update channel link for the bot
 
